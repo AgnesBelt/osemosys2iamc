5efd6814b230baca17c970b1e649285dceb9ddb9
Agnese <beltramo@kth.se>
2021-03-11 12:00:48 +0100

    updated config.yaml script for GLUCOSE + added script for unit conversion and generated results files in .csv

---
+++ pyam_UnitConversionGLUCOSE.py
@@ -0,0 +1,28 @@
+#%%
+
+import pandas as pd
+import pyam
+import pint
+
+Baseline_results=pd.read_csv('results/GLUCOSE_Baseline.csv')
+
+IAMC_glucose = pyam.IamDataFrame(Baseline_results)
+capacity=IAMC_glucose.convert_unit('TW', to='GW').timeseries()
+
+IAMC_glucose2 = pyam.IamDataFrame(capacity)
+emissions=IAMC_glucose2.convert_unit('Gt CO2e/yr', to='Mt CO2e/yr').timeseries()
+
+emissions.to_csv('results/GLUCOSE_Baseline_ConvertedUnits.csv')
+
+
+Total_results=pd.read_csv('results/GLUCOSE_Total.csv')
+
+IAMC_glucose = pyam.IamDataFrame(Total_results)
+capacity=IAMC_glucose.convert_unit('TW', to='GW').timeseries()
+
+IAMC_glucose2 = pyam.IamDataFrame(capacity)
+emissions=IAMC_glucose2.convert_unit('Gt CO2e/yr', to='Mt CO2e/yr').timeseries()
+
+emissions.to_csv('results/GLUCOSE_Total_ConvertedUnits.csv')
+
+# %%
