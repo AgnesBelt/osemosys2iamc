4f3a090bf68164d7cbc116ea3759622af7792c42
Agnese <beltramo@kth.se>
2021-03-26 09:25:26 +0100

    updated conversion script for GLUCOSE

--- pyam_UnitConversionGLUCOSE.py
+++ pyam_UnitConversionGLUCOSE.py
@@ -4,18 +4,18 @@ import pandas as pd
 import pyam
 import pint
 
-Baseline_results=pd.read_csv('results/GLUCOSE_Baseline.csv')
+# Baseline_results=pd.read_csv('results/GLUCOSE_Baseline.csv')
 
-IAMC_glucose = pyam.IamDataFrame(Baseline_results)
-capacity=IAMC_glucose.convert_unit('TW', to='GW').timeseries()
+# IAMC_glucose = pyam.IamDataFrame(Baseline_results)
+# capacity=IAMC_glucose.convert_unit('TW', to='GW').timeseries()
 
-IAMC_glucose2 = pyam.IamDataFrame(capacity)
-emissions=IAMC_glucose2.convert_unit('Gt CO2e/yr', to='Mt CO2e/yr').timeseries()
+# IAMC_glucose2 = pyam.IamDataFrame(capacity)
+# emissions=IAMC_glucose2.convert_unit('Gt CO2e/yr', to='Mt CO2e/yr').timeseries()
 
-emissions.to_csv('results/GLUCOSE_Baseline_ConvertedUnits.csv')
+# emissions.to_csv('results/GLUCOSE_Baseline_ConvertedUnits.csv')
 
 
-Total_results=pd.read_csv('results/GLUCOSE_Total.csv')
+Total_results=pd.read_csv('results/TotalCCG.csv')
 
 IAMC_glucose = pyam.IamDataFrame(Total_results)
 capacity=IAMC_glucose.convert_unit('TW', to='GW').timeseries()
@@ -23,6 +23,6 @@ capacity=IAMC_glucose.convert_unit('TW', to='GW').timeseries()
 IAMC_glucose2 = pyam.IamDataFrame(capacity)
 emissions=IAMC_glucose2.convert_unit('Gt CO2e/yr', to='Mt CO2e/yr').timeseries()
 
-emissions.to_csv('results/GLUCOSE_Total_ConvertedUnits.csv')
+emissions.to_csv('results/TotalCCG_ConvertedUnits.csv')
 
 # %%
